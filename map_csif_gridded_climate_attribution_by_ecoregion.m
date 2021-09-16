3d1e7546f03bae04285de3c6a490ab73d3debbbe
mpdannenberg <matthew-dannenberg@uiowa.edu>
2021-09-16 14:48:32 -0500

    Fix axes limits and number of digits in table

--- map_csif_gridded_climate_attribution_by_ecoregion.m
+++ map_csif_gridded_climate_attribution_by_ecoregion.m
@@ -24,7 +24,7 @@ eco_bounds(isnan(CSIF_obs) | isnan(eco_bounds)) = 0;
 
 %% Initiate table
 T = table('Size',[6 7], 'VariableTypes',{'string','string','string','string','string','string','string'},...
-    'VariableNames',{'Ecoregion','dCSIF_SMAP','dCSIF_All','dCSIF_PAR','dCSIF_SM','dCSIF_Tair','dCSIF_VPD'});
+    'VariableNames',{'Ecoregion','dCSIF','dCSIF_All','dCSIF_PAR','dCSIF_SM','dCSIF_Tair','dCSIF_VPD'});
 T.Ecoregion = {'Cold Deserts','Warm Deserts','Mediterranean California','Semiarid Prairies','Upper Gila Mountains','Sierra Madre Piedmont'}';
 
 %% Map
@@ -90,19 +90,19 @@ box off;
 ax = gca;
 ax.YAxis.Exponent = 0;
 set(gca, 'TickDir','out', 'TickLength',[0.02 0],...
-        'XLim',[0.25 5.75], 'FontSize',7, 'YLim',[-0.04 0.01])
+        'XLim',[0.25 5.75], 'FontSize',7, 'YLim',[-0.03 0.005])
 set(gca, 'XTickLabel',{'All','PAR','SM','T_{air}','VPD'})
 xtickangle(-25)
 title('Cold Deserts', 'FontSize',7)
 annotation('line',[0.22 0.4],[0.83 0.75], 'LineWidth',1);
 annotation('line',[0.22 0.43],[0.83 0.51], 'LineWidth',1);
 
-T.dCSIF_SMAP(1) = num2str(round(nanmean(CSIF_obs(ecoL2==10.1)), 4));
-T.dCSIF_All(1) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_all(ecoL2==10.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
-T.dCSIF_PAR(1) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_par(ecoL2==10.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
-T.dCSIF_SM(1) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_sm(ecoL2==10.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
-T.dCSIF_Tair(1) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_tair(ecoL2==10.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
-T.dCSIF_VPD(1) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_vpd(ecoL2==10.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
+T.dCSIF(1) = num2str(round(nanmean(CSIF_obs(ecoL2==10.1)), 3));
+T.dCSIF_All(1) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_all(ecoL2==10.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
+T.dCSIF_PAR(1) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_par(ecoL2==10.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
+T.dCSIF_SM(1) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_sm(ecoL2==10.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
+T.dCSIF_Tair(1) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_tair(ecoL2==10.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
+T.dCSIF_VPD(1) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_vpd(ecoL2==10.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
 
 % Mediterranean California
 h1 = axes('Parent', gcf, 'Position', [0.07 0.4 0.15 0.19]);
@@ -129,19 +129,19 @@ box off;
 ax = gca;
 ax.YAxis.Exponent = 0;
 set(gca, 'TickDir','out', 'TickLength',[0.02 0],...
-        'XLim',[0.25 5.75], 'FontSize',7, 'YLim',[-0.04 0.01])
+        'XLim',[0.25 5.75], 'FontSize',7, 'YLim',[-0.03 0.005])
 ylabel('Mean CSIF anomaly (mW m^{-2} nm^{-1} sr^{-1})', 'FontSize',8)
 set(gca, 'XTickLabel',{'All','PAR','SM','T_{air}','VPD'})
 xtickangle(-25)
 title('Mediterranean California', 'FontSize',7)
 annotation('line',[0.22 0.34],[0.51 0.39], 'LineWidth',1);
 
-T.dCSIF_SMAP(3) = num2str(round(nanmean(CSIF_obs(ecoL2==11.1)), 2));
-T.dCSIF_All(3) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_all(ecoL2==11.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
-T.dCSIF_PAR(3) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_par(ecoL2==11.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
-T.dCSIF_SM(3) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_sm(ecoL2==11.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
-T.dCSIF_Tair(3) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_tair(ecoL2==11.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
-T.dCSIF_VPD(3) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_vpd(ecoL2==11.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
+T.dCSIF(3) = num2str(round(nanmean(CSIF_obs(ecoL2==11.1)), 3));
+T.dCSIF_All(3) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_all(ecoL2==11.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
+T.dCSIF_PAR(3) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_par(ecoL2==11.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
+T.dCSIF_SM(3) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_sm(ecoL2==11.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
+T.dCSIF_Tair(3) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_tair(ecoL2==11.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
+T.dCSIF_VPD(3) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_vpd(ecoL2==11.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
 
 % Warm deserts
 h1 = axes('Parent', gcf, 'Position', [0.07 0.08 0.15 0.19]);
@@ -168,19 +168,19 @@ box off;
 ax = gca;
 ax.YAxis.Exponent = 0;
 set(gca, 'TickDir','out', 'TickLength',[0.02 0],...
-        'XLim',[0.25 5.75], 'FontSize',7, 'YLim',[-0.04 0.01])
+        'XLim',[0.25 5.75], 'FontSize',7, 'YLim',[-0.03 0.005])
 set(gca, 'XTickLabel',{'All','PAR','SM','T_{air}','VPD'})
 xtickangle(-25)
 title('Warm Deserts', 'FontSize',7)
 annotation('line',[0.22 0.45],[0.16 0.29], 'LineWidth',1);
 annotation('line',[0.22 0.68],[0.16 0.12], 'LineWidth',1);
 
-T.dCSIF_SMAP(2) = num2str(round(nanmean(CSIF_obs(ecoL2==10.2)), 2));
-T.dCSIF_All(2) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_all(ecoL2==10.2)), CSIF_all_ci(1), CSIF_all_ci(2)); 
-T.dCSIF_PAR(2) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_par(ecoL2==10.2)), CSIF_par_ci(1), CSIF_par_ci(2)); 
-T.dCSIF_SM(2) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_sm(ecoL2==10.2)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
-T.dCSIF_Tair(2) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_tair(ecoL2==10.2)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
-T.dCSIF_VPD(2) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_vpd(ecoL2==10.2)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
+T.dCSIF(2) = num2str(round(nanmean(CSIF_obs(ecoL2==10.2)), 3));
+T.dCSIF_All(2) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_all(ecoL2==10.2)), CSIF_all_ci(1), CSIF_all_ci(2)); 
+T.dCSIF_PAR(2) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_par(ecoL2==10.2)), CSIF_par_ci(1), CSIF_par_ci(2)); 
+T.dCSIF_SM(2) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_sm(ecoL2==10.2)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
+T.dCSIF_Tair(2) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_tair(ecoL2==10.2)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
+T.dCSIF_VPD(2) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_vpd(ecoL2==10.2)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
 
 % Semiarid prairies
 h1 = axes('Parent', gcf, 'Position', [0.78 0.72 0.15 0.19]);
@@ -206,19 +206,19 @@ hold off;
 box off;
 ax = gca;
 ax.YAxis.Exponent = 0;
-set(gca, 'TickDir','out', 'TickLength',[0.02 0], 'YLim',[-0.04 0.01],...
+set(gca, 'TickDir','out', 'TickLength',[0.02 0], 'YLim',[-0.03 0.005],...
         'XLim',[0.25 5.75], 'FontSize',7,'YAxisLocation','right')
 set(gca, 'XTickLabel',{'All','PAR','SM','T_{air}','VPD'})
 xtickangle(-25)
 title('Semiarid prairies', 'FontSize',7)
 annotation('line',[0.78 0.68],[0.78 0.45], 'LineWidth',1);
 
-T.dCSIF_SMAP(4) = num2str(round(nanmean(CSIF_obs(ecoL2==9.4)), 2));
-T.dCSIF_All(4) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_all(ecoL2==9.4)), CSIF_all_ci(1), CSIF_all_ci(2)); 
-T.dCSIF_PAR(4) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_par(ecoL2==9.4)), CSIF_par_ci(1), CSIF_par_ci(2)); 
-T.dCSIF_SM(4) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_sm(ecoL2==9.4)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
-T.dCSIF_Tair(4) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_tair(ecoL2==9.4)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
-T.dCSIF_VPD(4) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_vpd(ecoL2==9.4)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
+T.dCSIF(4) = num2str(round(nanmean(CSIF_obs(ecoL2==9.4)), 3));
+T.dCSIF_All(4) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_all(ecoL2==9.4)), CSIF_all_ci(1), CSIF_all_ci(2)); 
+T.dCSIF_PAR(4) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_par(ecoL2==9.4)), CSIF_par_ci(1), CSIF_par_ci(2)); 
+T.dCSIF_SM(4) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_sm(ecoL2==9.4)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
+T.dCSIF_Tair(4) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_tair(ecoL2==9.4)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
+T.dCSIF_VPD(4) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_vpd(ecoL2==9.4)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
 
 % Upper Gila Mountains
 h1 = axes('Parent', gcf, 'Position', [0.78 0.4 0.15 0.19]);
@@ -244,21 +244,20 @@ hold off;
 box off;
 ax = gca;
 ax.YAxis.Exponent = 0;
-set(gca, 'TickDir','out', 'TickLength',[0.02 0], 'YLim',[-0.04 0.01],...
-        'XLim',[0.25 5.75], 'FontSize',7, 'YLim',[-0.01 0.002],...
-        'YAxisLocation','right','YTickLabels',{'-0.01','','0'})
+set(gca, 'TickDir','out', 'TickLength',[0.02 0], 'YLim',[-0.03 0.005],...
+        'XLim',[0.25 5.75], 'FontSize',7,'YAxisLocation','right')
 ylabel('Mean CSIF anomaly (mW m^{-2} nm^{-1} sr^{-1})', 'FontSize',8)
 set(gca, 'XTickLabel',{'All','PAR','SM','T_{air}','VPD'})
 xtickangle(-25)
 title('Upper Gila Mountains', 'FontSize',7)
 annotation('line',[0.78 0.54],[0.46 0.255], 'LineWidth',1);
 
-T.dCSIF_SMAP(5) = num2str(round(nanmean(CSIF_obs(ecoL2==13.1)), 2));
-T.dCSIF_All(5) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_all(ecoL2==13.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
-T.dCSIF_PAR(5) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_par(ecoL2==13.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
-T.dCSIF_SM(5) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_sm(ecoL2==13.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
-T.dCSIF_Tair(5) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_tair(ecoL2==13.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
-T.dCSIF_VPD(5) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_vpd(ecoL2==13.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
+T.dCSIF(5) = num2str(round(nanmean(CSIF_obs(ecoL2==13.1)), 3));
+T.dCSIF_All(5) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_all(ecoL2==13.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
+T.dCSIF_PAR(5) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_par(ecoL2==13.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
+T.dCSIF_SM(5) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_sm(ecoL2==13.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
+T.dCSIF_Tair(5) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_tair(ecoL2==13.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
+T.dCSIF_VPD(5) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_vpd(ecoL2==13.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
 
 % Sierra Madre Piedmont
 h1 = axes('Parent', gcf, 'Position', [0.78 0.08 0.15 0.19]);
@@ -284,19 +283,19 @@ hold off;
 box off;
 ax = gca;
 ax.YAxis.Exponent = 0;
-set(gca, 'TickDir','out', 'TickLength',[0.02 0], 'YLim',[-0.04 0.01],...
+set(gca, 'TickDir','out', 'TickLength',[0.02 0], 'YLim',[-0.03 0.005],...
         'XLim',[0.25 5.75], 'FontSize',7,'YAxisLocation','right')
 set(gca, 'XTickLabel',{'All','PAR','SM','T_{air}','VPD'})
 xtickangle(-25)
 title('Sierra Madre Piedmont', 'FontSize',7)
 annotation('line',[0.78 0.55],[0.24 0.18], 'LineWidth',1);
 
-T.dCSIF_SMAP(6) = num2str(round(nanmean(CSIF_obs(ecoL2==12.1)), 2));
-T.dCSIF_All(6) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_all(ecoL2==12.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
-T.dCSIF_PAR(6) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_par(ecoL2==12.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
-T.dCSIF_SM(6) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_sm(ecoL2==12.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
-T.dCSIF_Tair(6) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_tair(ecoL2==12.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
-T.dCSIF_VPD(6) = sprintf('%.4f [%.4f, %.4f]', nanmean(CSIF_vpd(ecoL2==12.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
+T.dCSIF(6) = num2str(round(nanmean(CSIF_obs(ecoL2==12.1)), 3));
+T.dCSIF_All(6) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_all(ecoL2==12.1)), CSIF_all_ci(1), CSIF_all_ci(2)); 
+T.dCSIF_PAR(6) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_par(ecoL2==12.1)), CSIF_par_ci(1), CSIF_par_ci(2)); 
+T.dCSIF_SM(6) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_sm(ecoL2==12.1)), CSIF_sm_ci(1), CSIF_sm_ci(2)); 
+T.dCSIF_Tair(6) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_tair(ecoL2==12.1)), CSIF_tair_ci(1), CSIF_tair_ci(2)); 
+T.dCSIF_VPD(6) = sprintf('%.3f [%.3f, %.3f]', nanmean(CSIF_vpd(ecoL2==12.1)), CSIF_vpd_ci(1), CSIF_vpd_ci(2)); 
 
 %% Save figure and table
 set(gcf,'PaperPositionMode','auto')
